11b34a69a79c55fae5f37c50c05804d2bbbc7cf4
shubham92000 <sbhsingh616@gmail.com>
2022-10-20 22:31:49 +0530

    added password hashing

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,8 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator';
+import { BadRequestError } from '../errors/bad-request-error';
 import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { User } from '../models/user';
 
 const router = express.Router();
 
@@ -13,7 +14,7 @@ router.post('/api/users/signup', [
     .trim()
     .isLength({ min:4 , max:20 })
     .withMessage('password must be between 4 and 20 characters')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
   const errors = validationResult(req);
 
   if(!errors.isEmpty()){
@@ -22,10 +23,20 @@ router.post('/api/users/signup', [
 
   const { email, password } = req.body;
 
-  console.log('Create a new user');
-  throw new DatabaseConnectionError();
+  const existingUser = await User.findOne({ email });
 
-  res.send({});
+  if(existingUser){
+    throw new BadRequestError('Email in use')
+  }
+
+  // hash password
+  
+
+  const user = User.build({ email, password });
+  await user.save();
+
+  // jwt
+  res.status(201).send(user);
 });
 
 export { router as signupRouter };
