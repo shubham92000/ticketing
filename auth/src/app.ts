25108c4b062a9c53d6954f70ffb148bf3b45667a
shubham92000 <sbhsingh616@gmail.com>
2022-10-22 15:14:13 +0530

    auth index.js refactor

---
+++ auth/src/app.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import { currentuserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+import cookieSession from 'cookie-session';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+	cookieSession({
+		signed: false,
+		secure: true,
+	})
+);
+
+app.use(currentuserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+// sync
+// app.all('*', () => {
+//   throw new NotFoundError();
+// })
+
+// async -> ( callback, promise, async-await )
+// app.all('*', async (req, res, next) => {
+//   next(new NotFoundError());
+// })
+
+// using next instead of throw is annoying
+app.all('*', async (req, res) => {
+	throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,59 +1,20 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import { currentuserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(cookieSession({
-  signed: false,
-  secure: true
-}));
-
-app.use(currentuserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-// sync
-// app.all('*', () => {
-//   throw new NotFoundError();
-// })
-
-// async -> ( callback, promise, async-await )
-// app.all('*', async (req, res, next) => {
-//   next(new NotFoundError());
-// })
-
-// using next instead of throw is annoying
-app.all('*',async (req, res) => {
-  throw new NotFoundError();
-})
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
-  try {
-    if(!process.env.JWT_KEY){
-      throw new Error('JWT_KEY must be defined');
-    }
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-    console.log('Connected to auth mongodb');
-  } catch (err) {
-    console.error(err);
-  }
+	try {
+		if (!process.env.JWT_KEY) {
+			throw new Error('JWT_KEY must be defined');
+		}
+		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+		console.log('Connected to auth mongodb');
+	} catch (err) {
+		console.error(err);
+	}
 
-  app.listen(3000,() => {
-    console.log(`listening on port 3000!!!!!!`);
-  });
+	app.listen(3000, () => {
+		console.log(`listening on port 3000!!!!!!`);
+	});
 };
 
 start();
