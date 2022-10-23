7ed38be667e729a1962282c5431e50ea83890fc3
shubham92000 <sbhsingh616@gmail.com>
2022-10-23 12:35:09 +0530

    added signup page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,17 @@
-const Home = () => {
-	return <h1>index1</h1>;
+import buildClient from '../api/build-client';
+
+const Home = ({ currentUser }) => {
+	return currentUser ? (
+		<h1>You are signed in</h1>
+	) : (
+		<h1>You are not signed in</h1>
+	);
+};
+
+Home.getInitialProps = async (context) => {
+	const client = buildClient(context);
+	const { data } = await client.get('/api/users/currentuser');
+	return data;
 };
 
 export default Home;
