0ae634ea972e24ffd9506d5666d578e9692e0d89
shubham92000 <sbhsingh616@gmail.com>
2022-10-24 08:34:41 +0530

    added mongo env

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,7 +6,10 @@ const start = async () => {
 		if (!process.env.JWT_KEY) {
 			throw new Error('JWT_KEY must be defined');
 		}
-		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+		if (!process.env.MONGO_URI) {
+			throw new Error('MONGO_URI must be defined');
+		}
+		await mongoose.connect(process.env.MONGO_URI);
 		console.log('Connected to auth mongodb');
 	} catch (err) {
 		console.error(err);
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,8 +6,11 @@ const start = async () => {
 		if (!process.env.JWT_KEY) {
 			throw new Error('JWT_KEY must be defined');
 		}
-		await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-		console.log('Connected to auth mongodb');
+		if (!process.env.MONGO_URI) {
+			throw new Error('MONGO_URI must be defined');
+		}
+		await mongoose.connect(process.env.MONGO_URI);
+		console.log('Connected to tickets mongodb');
 	} catch (err) {
 		console.error(err);
 	}
