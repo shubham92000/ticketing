bbfc9510c889f9b9d6940f951d6c5989a9f00827
shubham92000 <sbhsingh616@gmail.com>
2022-10-25 15:34:39 +0530

    added natsWrapper

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,14 +1,24 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
+	if (!process.env.JWT_KEY) {
+		throw new Error('JWT_KEY must be defined');
+	}
+	if (!process.env.MONGO_URI) {
+		throw new Error('MONGO_URI must be defined');
+	}
 	try {
-		if (!process.env.JWT_KEY) {
-			throw new Error('JWT_KEY must be defined');
-		}
-		if (!process.env.MONGO_URI) {
-			throw new Error('MONGO_URI must be defined');
-		}
+		await natsWrapper.connect('ticketing', 'sdf', 'http://nats-srv:4222');
+
+		natsWrapper.client.on('close', () => {
+			console.log('tickets NATS connection closed');
+			process.exit();
+		});
+		process.on('SIGINT', () => natsWrapper.client.close());
+		process.on('SIGTERM', () => natsWrapper.client.close());
+
 		await mongoose.connect(process.env.MONGO_URI);
 		console.log('Connected to tickets mongodb');
 	} catch (err) {
