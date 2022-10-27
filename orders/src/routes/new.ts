6e540aaf0a1e230c33211545ee95b297c9ac0d74
shubham92000 <sbhsingh616@gmail.com>
2022-10-27 08:35:44 +0530

    publish events in new and delete route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -8,8 +8,10 @@ import {
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
 import { Order } from '../models/order';
 import { Ticket } from '../models/ticket';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
@@ -56,6 +58,16 @@ router.post(
 		await order.save();
 
 		// publish an event for order created
+		new OrderCreatedPublisher(natsWrapper.client).publish({
+			id: order.id,
+			status: order.status,
+			userId: order.userId,
+			expiresAt: order.expiresAt.toISOString(),
+			ticket: {
+				id: ticket.id,
+				price: ticket.price,
+			},
+		});
 
 		res.status(201).send(order);
 	}
