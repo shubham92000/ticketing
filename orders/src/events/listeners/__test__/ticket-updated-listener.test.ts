ea261643532e91d4fe420c0860245788fef11de7
shubham92000 <sbhsingh616@gmail.com>
2022-10-29 13:41:07 +0530

    added test for ticket created and updated listener

---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,69 @@
+import { TicketUpdatedEvent } from '@ticket-mssg/common';
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../../models/ticket';
+import { natsWrapper } from '../../../nats-wrapper';
+import { TicketUpdatedListener } from '../ticket-updated-listener';
+
+const setup = async () => {
+	// create listener
+	const listener = new TicketUpdatedListener(natsWrapper.client);
+
+	// create and save a ticket
+	const ticket = await Ticket.build({
+		id: new mongoose.Types.ObjectId().toHexString(),
+		title: 'concert',
+		price: 5,
+	});
+
+	await ticket.save();
+
+	// create fake data object
+
+	const data: TicketUpdatedEvent['data'] = {
+		id: ticket.id,
+		version: ticket.version + 1,
+		title: 'title',
+		price: 10,
+		userId: '123',
+	};
+
+	// create fake message object
+	// @ts-ignore
+	const msg: Message = {
+		ack: jest.fn(),
+	};
+
+	return { listener, data, msg, ticket };
+};
+
+it('finds, updated and saves a ticket', async () => {
+	const { msg, data, ticket, listener } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	const updatedTicket = await Ticket.findById(ticket.id);
+
+	expect(updatedTicket!.title).toEqual(data.title);
+	expect(updatedTicket!.price).toEqual(data.price);
+	expect(updatedTicket!.version).toEqual(data.version);
+});
+
+it('acks the message', async () => {
+	const { msg, data, ticket, listener } = await setup();
+
+	await listener.onMessage(data, msg);
+
+	expect(msg.ack).toHaveBeenCalled();
+});
+
+it('does not call ack if the event has a skipped version number', async () => {
+	const { msg, data, ticket, listener } = await setup();
+
+	data.version = 10;
+
+	try {
+		await listener.onMessage(data, msg);
+	} catch (err) {}
+	expect(msg.ack).not.toHaveBeenCalled();
+});
