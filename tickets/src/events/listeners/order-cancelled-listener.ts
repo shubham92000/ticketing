a6fccee5ed7915a32c3ccb34cfc50fcdd292ecbe
shubham92000 <sbhsingh616@gmail.com>
2022-10-30 07:08:06 +0530

    added order-created and order-cancelled listeners

--- tickets/src/events/listeners/order-cancelled-listener.ts
+++ tickets/src/events/listeners/order-cancelled-listener.ts
@@ -1,34 +1,32 @@
-import { Listener, OrderCreatedEvent, Subjects } from '@ticket-mssg/common';
+import { Listener, OrderCancelledEvent, Subjects } from '@ticket-mssg/common';
 import { Message } from 'node-nats-streaming';
 import { Ticket } from '../../models/ticket';
 import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 import { queueGroupName } from './queue-group-name';
 
-export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
-	readonly subject = Subjects.OrderCreated;
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+	readonly subject = Subjects.OrderCancelled;
 	queueGroupName = queueGroupName;
 
-	async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
-		// find the ticket that the order is reserving
+	async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
 		const ticket = await Ticket.findById(data.ticket.id);
 
 		if (!ticket) {
 			throw new Error('Ticket not found');
 		}
-		// mark ticket as reserved
-		ticket.set({ orderId: data.id });
+
+		ticket.set({ orderId: undefined });
 		await ticket.save();
 
 		await new TicketUpdatedPublisher(this.client).publish({
 			id: ticket.id,
-			version: ticket.version,
-			title: ticket.title,
-			price: ticket.price,
+			orderId: ticket.orderId,
 			userId: ticket.userId,
-			orderId: data.id,
+			price: ticket.price,
+			title: ticket.title,
+			version: ticket.version,
 		});
 
-		// ack
 		msg.ack();
 	}
 }
